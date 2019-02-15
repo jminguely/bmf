7fd42b00698adfce191de796af3edb693094923f
Julien Minguely <jminguely@gmail.com>
2019-02-15 15:49:57 -0500

    Best site is now better

--- assets/components/base.js
+++ assets/components/base.js
@@ -6,5 +6,15 @@ svgIcons(); // Must run as soon as possible
 (function ($) {
   $(document).ready(function () {
     layout();
+
+    $('.internal-link').click(function(e){
+      var targetId = $(this).attr('href').substr(1);
+
+      $('html, body').animate({
+          scrollTop: $(`#${targetId}`).offset().top
+      }, 1000, "swing");
+
+      e.preventDefault();
+    });
   });
 })(jQuery);
