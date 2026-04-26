9fde022af239e810b81b4ec896d71a4776c448e1
Julien Minguely <jminguely@gmail.com>
2026-04-26 20:51:18 +0200

    updates suisa list

--- web/parts/booking/pro.php
+++ web/parts/booking/pro.php
@@ -9,7 +9,7 @@
 
           <li><a href="build/docs/fiche-technique-hors-scene-2026.pdf" target="_blank">Fiche technique - Hors scène</a></li>
           <li><a href="build/docs/fiche-technique-sur-scene-2026.pdf" target="_blank">Fiche technique - Sur scène</a></li>
-          <li><a href="build/docs/liste-suisa-2024.pdf" target="_blank">Liste SUISA</a></li>
+          <li><a href="build/docs/liste-suisa-2026.pdf" target="_blank">Liste SUISA</a></li>
           <li><a href="build/docs/rider-2023.pdf" target="_blank">Rider</a></li>
           <li><a href="build/docs/bmf_photo_shoot_FINAL-1.jpg" target="_blank">Photo officielle</a></li>
         </ul>
